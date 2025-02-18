5830e323c8970b055fcfed15d53e1259f1c4aa4f
krisnadharma1412 <iputukrisnadharma@gmail.com>
2025-02-18 18:33:01 +0700

    Add Layout

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js, ts, jsx, tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    container: {
+      padding: {
+        md: "10rem",
+      },
+    },
   },
   plugins: [],
-}
-
+};
